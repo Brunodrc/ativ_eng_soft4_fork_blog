d5b4504a3d0fd75c3a9520e5649652b43d4936a2
Brunodrc <bruno.bdrc@gmail.com>
2023-03-22 09:38:05 -0300

    quinta tentativa, primeiro commit do segundo aplicativo blog

---
+++ smallblog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+admin.site.register(Post)
+
+""" Username: ola
+Email address: ola@example.com
+Password: 123ola"""
