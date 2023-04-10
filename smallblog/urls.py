0823d569d0f6224cc09319c214b05025d65b6d6a
Brunodrc <bruno.bdrc@gmail.com>
2023-04-10 20:04:22 -0300

    CRUD finalizado

--- smallblog/urls.py
+++ smallblog/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('new_post/', views.create_post, name='create_post'),
+    path('salve_post/', views.salve_post, name='salve_post'),
+    path('edit_post/<int:id_post>', views.edit_post, name='edit_post'),
+    path('save_edit/<int:id_post>', views.salve_edit, name='salve_edit'),
+    path('delete_post/<int:id_post>', views.delete_post, name='delete_post'),
 ]
--- smallblog/views.py
+++ smallblog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.utils import timezone
 from .models import Post
 
@@ -6,3 +7,39 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'smallblog/post_list.html', {'posts':posts})
+
+def create_post(request):
+    return render(request, 'smallblog/create_post.html')
+
+def salve_post(request):
+    title = request.POST.get('title')
+    text = request.POST.get('text')
+
+    post = Post(title=title, text=text)
+    post.save()
+
+    return redirect('post_list')
+
+def edit_post(request, id_post):
+    exist_post = Post.objects.get(pk=id_post)
+    
+    return render(request,'smallblog/edit_post.html',{'post':exist_post} )
+
+def salve_edit(request, id_post):
+    exist_post = Post.objects.get(pk=id_post)
+    title = request.POST.get('title')
+    text = request.POST.get('text')
+
+    exist_post.text = text
+    exist_post.title = title
+    # falta implementar a vizualização de updated updated = timezone.now()
+
+    exist_post.save()
+    return redirect('post_list')
+
+def delete_post(request, id_post):
+    exist_post = Post.objects.get(pk=id_post)
+    exist_post.delete()
+    return redirect('post_list')
+
+
